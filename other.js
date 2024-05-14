a5c373dd1e0b742498c0198c0b51eab551f6a180
claudiayin <160663516+claudiayin@users.noreply.github.com>
2024-05-14 21:28:23 +1200

    adjust strokeweight + message

--- other.js
+++ other.js
@@ -1,51 +1,53 @@
-    let isDrawing = false;
+let isDrawing = false;
     let bgMusic = document.getElementById("bgMusic");
-    let muteButton = document.getElementById("muteButton");
-    let strokeColor;
-
+    let playPauseButton = document.getElementById("playPauseButton");
+    let showMessage = true;
 
     function setup() {
       createCanvas(windowWidth, windowHeight);
       background(255);
-      strokeColor = color(0);
+      textSize(32);
+      textAlign(CENTER, CENTER);
     }
-
 
     function draw() {
       if (isDrawing) {
-        stroke(strokeColor);
-        strokeWeight(13);
+        stroke(0);
+        strokeWeight(10);
         line(pmouseX, pmouseY, mouseX, mouseY);
       }
+      if (showMessage) {
+        fill(0);
+        text("Colors will change with each cursor release", width/2, height/2);
+      }
     }
-
 
     function mousePressed() {
       isDrawing = true;
     }
 
-
     function mouseReleased() {
       isDrawing = false;
-      strokeColor = color(random(255), random(255), random(255));
+      showMessage = false; // Hide the message
+      setTimeout(() => {
+        showMessage = true; // Show the message again after 5 seconds
+      }, 5000);
     }
-
 
     function resetCanvas() {
       background(255);
     }
 
-
     function togglePlayPause() {
-        if (bgMusic.paused) {
-          bgMusic.play();
-          playPauseButton.innerText = "Pause"; 
-        } else {
-          bgMusic.pause();
-          playPauseButton.innerText = "Play"; 
-        }
+      if (bgMusic.paused) {
+        bgMusic.play();
+        playPauseButton.innerText = "Pause"; // Change button text to "Pause"
+      } else {
+        bgMusic.pause();
+        playPauseButton.innerText = "Play"; // Change button text to "Play"
       }
+    }
 
     function goBack() {
-        window.location.href = "index.html"; 
-      }
+      window.location.href = "index.html"; // Replace "index.html" with the URL of your home page
+    }
